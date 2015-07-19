a602c24cab584346a63366b0ab2f48e22de14cb6
Nick Sarten <gen.battle@gmail.com>
2015-07-19 12:52:45 +1200

    Componentized algorithm and improved tests.
    Split the part of the algorithm that determined which mean was closest
    to a given point to reduce the amount of code in the main kmeans
    algorithm a bit. Also improved the reliability of the kmeans test, and
    added some tests for the cases where the algorithm should panic.

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,34 @@ This create contains a simple implementation of the
 
 extern crate rand;
 
+// finds the point in the list of means that is closest to the given point and returns the index
+// of that mean.
+fn find_closest(point: &[f32; 2], means: &[[f32; 2]]) -> usize {
+    // find the mean that is closest
+    let mut distances = means.iter().map(|&m|{
+        let d0 = m[0] - point[0];
+        let d1 = m[1] - point[1];
+        d0 * d0 + d1 * d1
+    });
+    let mut min = distances.next().unwrap();
+    let mut index = 0;
+    // TODO: should be able to convert this to a fold or something?
+    for v in distances.enumerate() {
+        if v.1 < min {
+            min = v.1;
+            index = v.0 + 1;
+        }
+    }
+    return index;
+}
+
 /*
 The `kmeans` function provides a simple implementation of the standard k-means algorithm, as
 described [here](http://en.wikipedia.org/wiki/K-means_clustering#Standard_algorithm).
 */
-pub fn kmeans(data: &[[f32; 2]], k: usize) -> Vec<[f32; 2]> {
-    assert!(k >= 2); // this algorithm won't work with k < 2 at the moment
+pub fn kmeans(data: &[[f32; 2]], k: usize) -> Vec<[f32; 2]>  {
+    assert!(k > 1); // this algorithm won't work with k < 2 at the moment
+    assert!(data.len() > 1); // won't work with at least one data point
     // randomly select initial means from data set
     let mut rng = rand::thread_rng();
     let mut means: Vec<[f32; 2]> = rand::sample(&mut rng, data.iter(), k as usize).iter().map(|v|{*v.clone()}).collect();
@@ -22,22 +44,7 @@ pub fn kmeans(data: &[[f32; 2]], k: usize) -> Vec<[f32; 2]> {
         {
             // Assignment step
             let clusters = data.iter().map(|&d|{
-                // find the mean that is closest
-                let mut distances = means.iter().map(|&m|{
-                    let d0 = m[0] - d[0];
-                    let d1 = m[1] - d[1];
-                    d0 * d0 + d1 * d1
-                });
-                let mut min = distances.next().unwrap();
-                let mut index = 0;
-                // TODO: should be able to convert this to a fold or something?
-                for v in distances.enumerate() {
-                    if v.1 < min {
-                        min = v.1;
-                        index = v.0 + 1;
-                    }
-                }
-                return index;
+                find_closest(&d, &means)
             }).zip(data);
             // Update step
             let mut means_count: Vec<usize> = vec![0; k as usize];
@@ -62,18 +69,62 @@ pub fn kmeans(data: &[[f32; 2]], k: usize) -> Vec<[f32; 2]> {
     return means;
 }
 
-extern crate csv;
-#[test]
-fn test_kmeans() {
-    let mut data_reader = csv::Reader::from_file("data/iris.data").unwrap();
-    let mut data: Vec<[f32; 2]> = Vec::new();
-    for record in data_reader.decode() {
-        let (sl, _, pl, _, _): (f32, f32, f32, f32, String) = record.unwrap();
-        data.push([sl, pl]);
+#[cfg(test)]
+mod tests {
+    extern crate csv;
+    use super::kmeans;
+    
+    #[cfg(test)]
+    fn read_test_data() -> Vec<[f32; 2]> {
+        let mut data_reader = csv::Reader::from_file("data/iris.data").unwrap();
+        let mut data: Vec<[f32; 2]> = Vec::new();
+        for record in data_reader.decode() {
+            let (sl, _, pl, _, _): (f32, f32, f32, f32, String) = record.unwrap();
+            data.push([sl, pl]);
+        }
+        println!("Read external data:");
+        println!("{:?}", data);
+        return data;
     }
-    println!("Read external data:");
-    println!("{:?}", data);
-    let means = kmeans(&data[..], 3);
-    println!("Got means {:?}", means);
-    assert!(false);
+
+    /*
+    Test the kmeans method with a basic f32 dataset loaded from a CSV file (in this case iris.data).
+    
+    NOTE: This test just checks that the algorithm runs successfully; I haven't figured out a way
+    to check the validity of the data yet as this dataset has more than one local minima for this
+    algorithm. That doesn't mean one result is wrong, it's just the way the algorithm works.
+    */
+    #[test]
+    fn test_kmeans() {
+        let data = read_test_data();
+        let means = kmeans(&data[..], 3);
+        println!("Got means {:?}", means);
+    }
+
+    /*
+    Test that the algorithm panics when k < 2 is given.
+    */
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn test_min_k() {
+        let data = read_test_data();
+        let means = kmeans(&data[..], 1);
+        println!("Got means {:?}", means);
+        // Should panic at this point
+        println!("test_min_k failed, should have panicked");
+    }
+    
+    /*
+    Test that the algorithm panics when no data is provided.
+    TODO: improve this behaviour?
+    */
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn test_min_data() {
+        let data: Vec<[f32; 2]> = Vec::new();
+        let means = kmeans(&data, 3);
+        println!("Got means {:?}", means);
+        // Should panic at this point
+        println!("test_min_data failed, should have panicked");
+    }
 }
