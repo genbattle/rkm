467f1a357eee6fd6b5e50ecbe71b906c28d0894e
Nick Sarten <gen.battle@gmail.com>
2015-07-25 19:02:28 +1200

    Got generic find_closest working.
    Credit to Quxxy for helping me figure out the details and what I was
    doing wrong. Still need to sort out the main kmeans function.

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,35 @@ This create contains a simple implementation of the
 
 extern crate rand;
 
-// finds the point in the list of means that is closest to the given point and returns the index
-// of that mean.
-fn find_closest(point: &[f32; 2], means: &[[f32; 2]]) -> usize {
+use std::ops::{Add, Sub, Mul, Div, Index};
+use std::cmp::{PartialOrd, PartialEq};
+use std::fmt::Debug;
+
+/*
+Numeric value trait, defines the types that can be used for the value of each dimension in a
+data point.
+*/
+trait Num: Add + Sub + Mul + Div + PartialOrd + PartialEq + Copy + Debug {}
+impl<T> Num for T where T: Add + Sub + Mul + Div + PartialOrd + PartialEq + Copy + Debug {}
+
+// kmeans data, for accessing 
+trait Data<T> where T: Index<usize> {
+    type V: Num = T;
+    
+    fn len() -> usize;
+}
+
+/*
+Find the point in the list of means that is closest to the given point and return the index
+of that mean in the means slice.
+*/
+fn find_closest<T, U>(point: &T, means: &[T]) -> usize
+where
+    T: Index<usize, Output=U>,
+    U: Add<Output=U> + Clone + Mul<Output=U> + PartialOrd + Sub<Output=U>
+{
     // find the mean that is closest
-    let mut distances = means.iter().map(|&m|{
+    let mut distances = means.iter().map(|&m|{ // TODO: make this part of the data trait? At the very least T will need to be Iterator
         let d0 = m[0] - point[0];
         let d1 = m[1] - point[1];
         d0 * d0 + d1 * d1
@@ -30,30 +54,35 @@ fn find_closest(point: &[f32; 2], means: &[[f32; 2]]) -> usize {
 The `kmeans` function provides a simple implementation of the standard k-means algorithm, as
 described [here](http://en.wikipedia.org/wiki/K-means_clustering#Standard_algorithm).
 */
-pub fn kmeans(data: &[[f32; 2]], k: usize) -> Vec<[f32; 2]>  {
+pub fn kmeans<T, U>(data: &[T], k: usize) -> Vec<T>
+where
+    T: Index<usize, Output=U>,
+    U: Add<Output=U> + Sub<Output=U> + Mul<Output=U> + Div<Output=U> + PartialOrd + PartialEq + Copy + Debug {
     assert!(k > 1); // this algorithm won't work with k < 2 at the moment
     assert!(data.len() > 1); // won't work with at least one data point
     // randomly select initial means from data set
     let mut rng = rand::thread_rng();
-    let mut means: Vec<[f32; 2]> = rand::sample(&mut rng, data.iter(), k).iter().map(|v|{*v.clone()}).collect();
+    let mut means: Vec<T> = rand::sample(&mut rng, data.iter(), k).iter().map(|v|{*v.clone()}).collect();
     let mut converged = false;
     // loop until convergence is reached
     while !converged {
-        let mut means_new: Vec<[f32; 2]> = vec![[0.0, 0.0]; k];
+        let mut means_new: Vec<T>;
         {
+            // TODO: put some of this code in the data trait?
             // Assignment step
             let clusters = data.iter().map(|&d|{
                 find_closest(&d, &means)
             }).zip(data);
             // Update step
             let mut means_count: Vec<usize> = vec![0; k];
+            for _ in [0..]
             for v in clusters {
                 means_new[v.0][0] += v.1[0];
                 means_new[v.0][1] += v.1[1];
                 means_count[v.0] += 1;
             }
             means_new = means_new.iter().zip(means_count.iter()).map(|v| {
-                [v.0[0] / *v.1 as f32, v.0[1] / *v.1 as f32]
+                [v.0[0] / *v.1, v.0[1] / *v.1]
             }).collect();
         }
         if means_new.iter().zip(means.iter()).all(|v| {
@@ -72,7 +101,6 @@ mod tests {
     extern crate csv;
     use super::kmeans;
     
-    #[cfg(test)]
     fn read_test_data() -> Vec<[f32; 2]> {
         let mut data_reader = csv::Reader::from_file("data/iris.data").unwrap();
         let mut data: Vec<[f32; 2]> = Vec::new();
