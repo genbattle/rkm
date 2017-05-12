ceccc45d89492e944775c15390767e392f0acb94
Nick Sarten <gen.battle@gmail.com>
2017-05-12 23:11:36 +1200

    Updated dependencies and got distance working.
    This project has been on haitus for a while, but I've finally got round
    to updating it. It helps that Rust and the ndarray library have made
    great leaps and bounds in the meantime.

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,9 @@ use std::ops::{Add, Sub, Mul, Div, Index};
 use std::cmp::{PartialOrd, PartialEq};
 use std::fmt::Debug;
 use std::iter::FromIterator;
-use ndarray::{Array, Dimension, Ix};
+use ndarray::{Array1, Array2, arr1, arr2};
+use rand::Rng;
+use rand::distributions::{Weighted, WeightedChoice};
 
 /*
 Numeric value trait, defines the types that can be used for the value of each dimension in a
@@ -20,43 +22,92 @@ data point.
 pub trait Value: num::Signed/* + From<usize>*/ + PartialOrd + Copy + Debug {}
 impl<T> Value for T where T: num::Signed/*+ From<usize>*/ + PartialOrd + Copy + Debug {}
 
-fn distance_squared<V: Value>(point_a: &Array<V, Ix>, point_b: &Array<V, Ix>) -> V {
+/*
+Find the distance between two data points, given as Array rows.
+*/
+fn distance_squared<V: Value>(point_a: &Array1<V>, point_b: &Array1<V>) -> V {
     point_a.iter().zip(point_b.iter()).fold(num::Zero::zero(), |acc, v| {
         let delta = *v.0 - *v.1;
         return acc + (delta * delta);
     })
 }
 
-fn closest_distance<V: Value>(means: &Array<V, (Ix, Ix)>, data: &Array<V, (Ix, Ix)>, k: u32) -> Vec<V> {
-    let mut distances = Vec::with_capacity(k as usize);
-    for i in 0..data.dim().0 {
-        let mut closest = distance_squared(&data.subview(0, i), &means.subview(0, 0));
-        for j in 0..means.dim().0 {
-            let distance = distance_squared(&data.subview(0, i), &means.subview(0, j));
-            if distance < closest {
-                closest = distance;
-            }
-        }
-        distances.push(closest);
-    }
-    return distances;
-}
+// /*
+// Find the shortest distance between each data point and any of a set of mean points.
+// TODO: This will be used for km++ initialization
+// TODO: This will need to return rand::distributions::Weighted in order to work
+// */
+// fn closest_distance<V: Value>(means: &Array2<V>, data: &Array2<V>, k: u32) -> Vec<V> {
+//     let mut distances = Vec::with_capacity(k as usize);
+//     for i in 0..data.dim().0 {
+//         let mut closest = distance_squared(&data.subview(0, i), &means.subview(0, 0));
+//         for j in 0..means.dim().0 {
+//             let distance = distance_squared(&data.subview(0, i), &means.subview(0, j));
+//             if distance < closest {
+//                 closest = distance;
+//             }
+//         }
+//         distances.push(closest);
+//     }
+//     return distances;
+// }
+
+// // TODO: kmeans++ initialization
+// /*
+// This is a mean initialization method based on the [kmeans++](https://en.wikipedia.org/wiki/K-means%2B%2B)
+// initialization algorithm.
+// */
+// fn initialize_plusplus<V: Value>(data: &Array2<V>, k: u32) -> Array2<V> {
+//     assert!(k > 0);
+//     let mut means = Array::<V, (Ix, Ix)>::zeros((k, data.dim().1));
+//     let mut rng = rand::thread_rng();
+//     let data_len = data.dim().0;
+//     means.subview(0, 0).assign(&data.subview(0, rng.gen_range(0, data_len)));
+//     for i in 1..data_len {
+// 		// Calculate the distance to the closest mean for each data point
+//         let distances = closest_distance(&means.slice(&[Si(0, Some(i as i32), 1), S]), data, k);
+// 		// Pick a random point weighted by the distance from existing means
+//         let weights = distances.iter().zip(0..data_len).map(|d|{
+//             Weighted{weight: *d.0, item: d.1}
+//         }).collect();
+//     }
+//     return means;
+// }
+
+// /*
+// Find the closest mean to a particular data point.
+// */
+// fn closest_mean<V: Value>(point: &ArrayView1<V>, means: &ArrayView2<V>) -> Ix {
+//     // TODO: Check dimensionality/size of means and point
+//     let mut shortest_distance = distance_squared(point, &means.subview(0, 0));
+//     let mut index: Ix = 0;
+//     for i in 0..means.dim().0 {
+//         let distance = distance_squared(point, &means.subview(0, i));
+//         if distance < shortest_distance {
+//             shortest_distance = distance;
+//             index = i;
+//         }
+//     }
+//     return index;
+// }
 
 #[cfg(test)]
 mod tests {
-    extern crate ndarray;
     // extern crate csv;
     // use super::kmeans;
-    use ndarray::Array;
     
     #[test]
     fn test_distance() {
+        use ndarray::arr1;
         use super::distance_squared;
-        let a = Array::from_vec(vec![1.0f32, 1.0f32]);
-        let b = Array::from_vec(vec![2.0f32, 2.0f32]);
-        let c = Array::from_vec(vec![1200.0f32, 1200.0f32]);
+        let a = arr1(&[1.0f32, 1.0f32]);
+        let b = arr1(&[2.0f32, 2.0f32]);
+        let c = arr1(&[1200.0f32, 1200.0f32]);
+        let d = arr1(&[1, 1]);
+        let e = arr1(&[1200, 1200]);
         assert_eq!(distance_squared(&a, &b), 2.0f32);
         assert_eq!(distance_squared(&a, &c), 2875202.0f32);
+        assert_eq!(distance_squared(&d, &e), 2875202);
     }
     
     // fn read_test_data() -> Vec<[f32; 2]> {
