1d03a1dc77f83f540ccb0c06c77a3550259fa678
Nick Sarten <gen.battle@gmail.com>
2019-08-11 21:34:50 +1200

    Formatted code with rustfmt.

--- src/example.rs
+++ src/example.rs
@@ -1,11 +1,11 @@
-extern crate rkm;
-extern crate ndarray;
 extern crate csv;
+extern crate ndarray;
+extern crate rkm;
 
 use ndarray::Array2;
 use std::str::FromStr;
 
-fn read_test_data() -> Array2<f32> {;
+fn read_test_data() -> Array2<f32> {
     let mut data_reader = csv::Reader::from_path("data/iris.data.csv").unwrap();
     let mut data: Vec<f32> = Vec::new();
     for record in data_reader.records() {
@@ -17,9 +17,11 @@ fn read_test_data() -> Array2<f32> {;
     Array2::from_shape_vec((data.len() / 2, 2), data).unwrap()
 }
 
-
 pub fn main() {
     let data = read_test_data();
     let (means, clusters) = rkm::kmeans_lloyd(&data.view(), 3, None);
-    println!("data:\n{:?}\nmeans:\n{:?}\nclusters:\n{:?}", data, means, clusters);
+    println!(
+        "data:\n{:?}\nmeans:\n{:?}\nclusters:\n{:?}",
+        data, means, clusters
+    );
 }
