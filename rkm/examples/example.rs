69e605f4c8a3edf8756731fba24e901d9a9bcae1
Nick Sarten <gen.battle@gmail.com>
2020-02-04 23:12:09 +1300

    Improved example plot
    New colors which will hopefully be more visible, and split data into
    subsets by cluster.

--- rkm/examples/example.rs
+++ rkm/examples/example.rs
@@ -1,7 +1,7 @@
 /// example.rs - a basic example that loads the simple iris data set.
 ///
 /// The data set is fed into the k-means algorithm and then the means
-/// and clusters are printed. TODO: a better output visualisation.
+/// and clusters are printed and plotted to a file.
 ///
 /// This example must be run from the crate root for the relative paths
 /// for the example data to be correct; this program will panic if the
@@ -9,9 +9,9 @@
 ///
 /// You can run this program with the cargo run command:
 /// `cargo run --example example`
-use ndarray::{Array2, ArrayView2};
-use std::str::FromStr;
+use ndarray::{Array2, ArrayView1, ArrayView2};
 use plotters::prelude::*;
+use std::str::FromStr;
 
 fn read_test_data() -> Array2<f32> {
     let mut data_reader = csv::Reader::from_path("data/iris.data.csv").unwrap();
@@ -25,9 +25,33 @@ fn read_test_data() -> Array2<f32> {
     Array2::from_shape_vec((data.len() / 2, 2), data).unwrap()
 }
 
+fn separate_groups<'a>(
+    data: &'a ArrayView2<f32>,
+    clusters: &[usize],
+) -> (
+    Vec<ArrayView1<'a, f32>>,
+    Vec<ArrayView1<'a, f32>>,
+    Vec<ArrayView1<'a, f32>>,
+) {
+    data.outer_iter().zip(clusters.iter()).fold(
+        (Vec::new(), Vec::new(), Vec::new()),
+        |mut state, (point, &cluster)| {
+            match cluster {
+                0 => state.0.push(point),
+                1 => state.1.push(point),
+                2 => state.2.push(point),
+                _ => (),
+            }
+            state
+        },
+    )
+}
+
 fn plot_means_clusters(data: &ArrayView2<f32>, means: &ArrayView2<f32>, clusters: &[usize]) {
     let root_area = BitMapBackend::new("test.png", (800, 600)).into_drawing_area();
     root_area.fill(&WHITE).unwrap();
+
+    let groups = separate_groups(data, clusters);
 
     let mut context = ChartBuilder::on(&root_area)
         .set_label_area_size(LabelAreaPosition::Left, 40)
@@ -35,13 +59,38 @@ fn plot_means_clusters(data: &ArrayView2<f32>, means: &ArrayView2<f32>, clusters
         .caption("K Means Demo", ("Arial", 40))
         .build_ranged(1.8f32..5.0f32, 0.0f32..3.0f32)
         .unwrap();
-    
+
     context.configure_mesh().draw().unwrap();
-    context.draw_series(data.outer_iter()
-        .map(|coords| Circle::new((coords[0], coords[1]), 3, GREEN.filled())))
+    context
+        .draw_series(
+            groups
+                .0
+                .iter()
+                .map(|coords| Circle::new(((*coords)[0], (*coords)[1]), 3, RGBColor(100, 140, 220).filled())),
+        )
         .unwrap();
-    context.draw_series(means.outer_iter()
-        .map(|coords| Circle::new((coords[0], coords[1]), 3, RED.filled())))
+    context
+        .draw_series(
+            groups
+                .1
+                .iter()
+                .map(|coords| Circle::new(((*coords)[0], (*coords)[1]), 3, RGBColor(120, 80, 30).filled())),
+        )
+        .unwrap();
+    context
+        .draw_series(
+            groups
+                .2
+                .iter()
+                .map(|coords| Circle::new(((*coords)[0], (*coords)[1]), 3, RGBColor(120, 240, 70).filled())),
+        )
+        .unwrap();
+    context
+        .draw_series(
+            means
+                .outer_iter()
+                .map(|coords| TriangleMarker::new((coords[0], coords[1]), 8, RED.filled())),
+        )
         .unwrap();
 }
 
