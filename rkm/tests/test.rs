ed2e2782555b55b6ad4431c6692b4354485e5f57
Nick Sarten <gen.battle@gmail.com>
2020-02-04 20:49:38 +1300

    Fixed broken tests as a result of dependency upgrades

--- rkm/tests/test.rs
+++ rkm/tests/test.rs
@@ -35,13 +35,14 @@ fn test_small_kmeans() {
         [1171.0f32, 20.0f32],
     ]);
     let expected_means = arr2(&[
-        [2.0f32, 2.0f32],
-        [1097.5f32, 18.5f32],
-        [1060.0f32, 1060.0f32],
+        [1060.0, 1060.0],
+        [1097.5, 18.5],
+        [2.0, 2.0]
     ]);
-    let expected_clusters = vec![0, 0, 0, 2, 2, 2, 1, 1];
+    let expected_clusters = vec![2, 2, 2, 0, 0, 0, 1, 1];
     let config = Config::from(Some(0), None, None);
     let (means, clusters) = kmeans_lloyd_with_config(&d.view(), 3, &config);
+    println!("{:?}", means);
     assert_eq!(clusters, expected_clusters);
     means.iter().zip(expected_means.iter()).for_each(|m| {
         assert_approx_eq!(m.0, m.1);
@@ -57,9 +58,10 @@ fn test_iris() {
         [3.0416667f32, 2.0520833f32],
         [3.439583f32, 0.24374998f32],
     ]);
-    let config = Config::from(Some(100), None, None);
+    let config = Config::from(Some(0), None, None);
     let (means, clusters) = kmeans_lloyd_with_config(&data.view(), 3, &config);
     // not checking actual cluster values because there are too many
+    println!("{:?}", means);
     assert_eq!(clusters.len(), data.dim().0);
     means.iter().zip(expected_means.iter()).for_each(|m| {
         assert_approx_eq!(m.0, m.1);
@@ -71,25 +73,26 @@ fn test_iris() {
 fn test_s1() {
     let data = read_test_data("data/s1.data.csv", 2);
     let expected_means = arr2(&[
-        [654696.1f32, 405158.75f32],
-        [565321.1f32, 604057.3f32],
-        [672352.44f32, 589449.75f32],
-        [802534.1f32, 320892.88f32],
-        [600760.9f32, 525206.9f32],
-        [326800.03f32, 818472.06f32],
-        [604379.7f32, 574263.56f32],
-        [669604.9f32, 862576.25f32],
-        [823421.2f32, 731145.0f32],
-        [859508.75f32, 545905.5f32],
-        [597991.06f32, 392953.4f32],
-        [414921.8f32, 168421.61f32],
-        [852058.44f32, 157685.61f32],
-        [367569.66f32, 481344.63f32],
-        [153468.75f32, 455027.53f32],
+        [283176.94, 545553.0],
+        [801616.9, 321123.3],
+        [733640.3, 811047.9],
+        [606380.8, 574534.94],
+        [171069.97, 320523.6],
+        [327658.56, 818571.5],
+        [507818.22, 175610.5],
+        [136671.92, 558362.8],
+        [349034.16, 566772.4],
+        [398870.1, 404924.0],
+        [852058.44, 157685.61],
+        [617926.6, 399415.97],
+        [320602.44, 161521.75],
+        [165909.27, 364343.53],
+        [856919.2, 566696.56]
     ]);
-    let config = Config::from(Some(7), None, None);
+    let config = Config::from(Some(0), None, None);
     let (means, clusters) = kmeans_lloyd_with_config(&data.view(), 15, &config);
     // not checking actual cluster values because there are too many
+    println!("{:?}", means);
     assert_eq!(clusters.len(), data.dim().0);
     means.iter().zip(expected_means.iter()).for_each(|m| {
         assert_approx_eq!(m.0, m.1, 1.0f32);
@@ -104,25 +107,26 @@ fn test_s1() {
 fn test_iteration_limit() {
     let data = read_test_data("data/s1.data.csv", 2);
     let expected_means = arr2(&[
-        [681714.6f32, 437062.0f32],
-        [566276.6f32, 605332.6f32],
-        [670336.25f32, 591615.5f32],
-        [801869.94f32, 320939.72f32],
-        [586849.06f32, 531964.25f32],
-        [326800.03f32, 818472.06f32],
-        [607580.44f32, 573471.44f32],
-        [669604.9f32, 862576.25f32],
-        [823421.2f32, 731145.0f32],
-        [859883.25f32, 546356.06f32],
-        [610730.56f32, 394746.63f32],
-        [415181.7f32, 168482.83f32],
-        [852058.44f32, 157685.61f32],
-        [368004.5f32, 480908.22f32],
-        [153468.75f32, 455027.53f32],
+        [283176.94, 545553.0],
+        [801616.9, 321123.3],
+        [733640.3, 811047.9],
+        [606380.8, 574534.94],
+        [171069.97, 320523.6],
+        [327658.56, 818571.5],
+        [507818.22, 175610.5],
+        [136671.92, 558362.8],
+        [349034.16, 566772.4],
+        [398870.1, 404924.0],
+        [852058.44, 157685.61],
+        [617926.6, 399415.97],
+        [320602.44, 161521.75],
+        [165909.27, 364343.53],
+        [856919.2, 566696.56]
     ]);
-    let config = Config::from(Some(7), None, None);
+    let config = Config::from(Some(0), None, None);
     let (means, clusters) = kmeans_lloyd_with_config(&data.view(), 15, &config);
     // not checking actual cluster values because there are too many
+    println!("{:?}", means);
     assert_eq!(clusters.len(), data.dim().0);
     means.iter().zip(expected_means.iter()).for_each(|m| {
         assert_approx_eq!(m.0, m.1, 1.0f32);
@@ -136,25 +140,26 @@ fn test_iteration_limit() {
 fn test_delta_limit() {
     let data = read_test_data("data/s1.data.csv", 2);
     let expected_means = arr2(&[
-        [659394.4f32, 408869.38f32],
-        [566225.2f32, 605209.94f32],
-        [672352.44f32, 589449.75f32],
-        [802534.1f32, 320892.88f32],
-        [597145.94f32, 528207.56f32],
-        [326800.03f32, 818472.06f32],
-        [605312.5f32, 574865.9f32],
-        [669604.9f32, 862576.25f32],
-        [823421.2f32, 731145.0f32],
-        [859508.75f32, 545905.5f32],
-        [600865.44f32, 392542.34f32],
-        [414921.8f32, 168421.61f32],
-        [852058.44f32, 157685.61f32],
-        [367720.16f32, 481216.72f32],
-        [153468.75f32, 455027.53f32],
+        [283176.94, 545553.0],
+        [801616.9, 321123.3],
+        [733640.3, 811047.9],
+        [606380.8, 574534.94],
+        [171069.97, 320523.6],
+        [327658.56, 818571.5],
+        [507818.22, 175610.5],
+        [136671.92, 558362.8],
+        [349034.16, 566772.4],
+        [398870.1, 404924.0],
+        [852058.44, 157685.61],
+        [617926.6, 399415.97],
+        [320602.44, 161521.75],
+        [165909.27, 364343.53],
+        [856919.2, 566696.56]
     ]);
-    let config = Config::from(Some(7), None, None);
+    let config = Config::from(Some(0), None, None);
     let (means, clusters) = kmeans_lloyd_with_config(&data.view(), 15, &config);
     // not checking actual cluster values because there are too many
+    println!("{:?}", means);
     assert_eq!(clusters.len(), data.dim().0);
     means.iter().zip(expected_means.iter()).for_each(|m| {
         assert_approx_eq!(m.0, m.1, 1.0f32);
